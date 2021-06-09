a9e491f6757ed15f7c402b14511b86dc94cbad01
Alfredo Bonilla <brolag@live.com>
2021-06-08 22:28:26 -0600

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,24 @@
+import { useEffect, useState } from "react";
+import Web3 from "web3";
+
 export default function Home() {
-  return <div className="bg-blue-900">hola mundo</div>
+  const [account, setAccount] = useState({ account: "" });
+
+  useEffect(() => {
+    // const fecthAddress = async () => {
+    //   const web3 = new Web3(Web3.givenProvider || "http://localhost:3000");
+    //   const network = await web3.eth.net.getNetworkType();
+    //   console.log(network); // should give you main if you're connected to the main network via metamask...
+    //   const accounts = await web3.eth.accounts;
+    //   setAccount({ account: accounts[0] });
+    //   console.log(accounts);
+    // };
+    // fecthAddress();
+  }, []);
+
+  return account ? (
+    <div className="bg-green-300">{account.account}</div>
+  ) : (
+    <div> no account </div>
+  );
 }
