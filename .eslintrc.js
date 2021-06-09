46518bc1b88d1fec4e3b223940b2f823416d324c
Alfredo Bonilla <brolag@live.com>
2021-06-09 08:53:20 -0600

    Func: add connection to ethereum network

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,37 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "plugin:react/recommended",
-        "standard",
-        "prettier"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true,
-            "tsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-        "prettier"
-    ],
-    "rules": {
-        "prettier/prettier": "error",
-        "no-unused-vars": "warn",
-        "no-console": "off",
-        "func-names": "off",
-        "no-process-exit": "off",
-        "object-shorthand": "off",
-        "class-methods-use-this": "off"
-    }
+  "env": {
+      "browser": true,
+      "es2021": true,
+      "node": true
+  },
+  "extends": [
+      "plugin:react/recommended",
+      "plugin:tailwind/recommended",
+      "standard",
+      "prettier"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true,
+          "tsx": true
+      },
+      "ecmaVersion": 12,
+      "sourceType": "module"
+  },
+  "plugins": [
+      "react",
+      "@typescript-eslint",
+      "prettier"
+  ],
+  "rules": {
+      "prettier/prettier": "error",
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "func-names": "off",
+      "no-process-exit": "off",
+      "object-shorthand": "off",
+      "class-methods-use-this": "off",
+      "semi": ["error", "never"],
+  }
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,25 @@
-import { useEffect, useState } from "react";
-import Web3 from "web3";
+import { useEffect, useState } from "react"
+import Web3 from "web3"
 
 export default function Home() {
-  const [account, setAccount] = useState({ account: "" });
+  const [account, setAccount] = useState({ account: "" })
 
   useEffect(() => {
-    // const fecthAddress = async () => {
-    //   const web3 = new Web3(Web3.givenProvider || "http://localhost:3000");
-    //   const network = await web3.eth.net.getNetworkType();
-    //   console.log(network); // should give you main if you're connected to the main network via metamask...
-    //   const accounts = await web3.eth.accounts;
-    //   setAccount({ account: accounts[0] });
-    //   console.log(accounts);
-    // };
-    // fecthAddress();
-  }, []);
+    const fecthAddress = async () => {
+      const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+      const network = await web3.eth.net.getNetworkType()
+      const accounts = await web3.eth.getAccounts()
+      console.log(network) // should give you main if you're connected to the main network via metamask...
+      setAccount({ account: accounts[0] })
+    }
+    fecthAddress()
+  }, [])
 
-  return <div>hello world</div>;
+  console.log(account)
+
+  return account && account.account ? (
+    <div>{account.account}</div>
+  ) : (
+    <div>not connected</div>
+  )
 }
